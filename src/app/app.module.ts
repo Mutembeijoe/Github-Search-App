0595dc6d0ef6883149f6cd1916f90037c28b7a44
mutembeijoe <mutembeijoe@gmail.com>
2019-09-26 11:20:54 +0300

    adds a service for calling the github api and registers it to the providers in app root module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GithubUserComponent } from './components/github-user/github-user.component';
+import { GithubService } from './services/github.service';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,9 @@ import { GithubUserComponent } from './components/github-user/github-user.compon
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    GithubService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
