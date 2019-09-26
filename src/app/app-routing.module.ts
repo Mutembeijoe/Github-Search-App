b2e30ce337c17ca0de8ef25d681b200f79fb4364
mutembeijoe <mutembeijoe@gmail.com>
2019-09-26 11:36:34 +0300

    adds Angular routing, with a home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GithubUserComponent } from './components/github-user/github-user.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: GithubUserComponent },
+  { path: '', redirectTo: 'home' , pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
