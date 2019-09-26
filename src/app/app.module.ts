e08f001ac5c6972262f4ad506def8fdfef04fab3
mutembeijoe <mutembeijoe@gmail.com>
2019-09-26 11:09:46 +0300

    adds a github-user component that will display a users info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GithubUserComponent } from './components/github-user/github-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GithubUserComponent
   ],
   imports: [
     BrowserModule,
